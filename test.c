ef4caa07955f5346c5a945aca178473c7d7ad09b
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2024-09-05 13:26:43 -0400

    finish everything

--- test.c
+++ test.c
@@ -1,124 +1,69 @@
-/* Fibonacci Series */
-#include <stdio.h>
-#include <string.h>
-void clearInputBuffer(void) 
-{ 
-    // Discard all remaining char's from the standard input buffer: 
-    while (getchar() != '\n') 
-    { 
-        ; // do nothing! 
-    } 
-} 
- 
-// Wait for user to input the "enter" key to continue 
-void suspend(void) 
-{ 
-    printf("<ENTER> to continue..."); 
-    clearInputBuffer(); 
-    putchar('\n'); 
-}
 
-void inputCString(char* inputString, int minRange, int maxRange)
+   #include <stdio.h>
+
+
+   #define WORD_LEN 15
+   #define VOCAL_LEN 100
+
+
+    struct Vocal
+   {
+      char word[WORD_LEN + 1];
+      // How long to hold the note (in seconds)
+      int sustainTime;  
+   };
+   struct Song
+   {
+      char title[WORD_LEN + 1];
+      char artist[WORD_LEN + 1];
+      struct Vocal lyrics[VOCAL_LEN];
+      int words;
+   };
+
+   int karaoke(const struct Song* song)
+   {
+      int i, tSeconds = 0;
+      struct Vocal word;
+      printf("%s (by: %s)\n\n", song->title, song->artist);
+      for (i = 0; i < song->words; i++)
+      {
+          word = song->lyrics[i];
+            printf("%s ", word.word);
+          if (word.sustainTime == 4)
+          {
+              puts("...");
+          }
+          tSeconds += word.sustainTime;
+      }
+
+      return tSeconds;
+   }    
+
+   void convertMinSec(const int seconds, int* min, int* sec)
+   {
+      *min = seconds / 60;
+      *sec = seconds % 60;
+   }
+
+int main(void)
 {
-    char str[20];
-    int i;
-    do
-    {
-        scanf("%[^\n]%*c", str);
-        i=0;
-        while(str[i]!='\0')
-        {
-            i++;
-        }
-        if(i == minRange == maxRange)
-        {
-            printf("ERROR: String length must be exactly %d chars: ",minRange);
-
-        }
-        else if (i < minRange)
-        {
-            printf("ERROR: String length must be between %d and %d chars: ",minRange,maxRange);
-        }
-        else if (i> maxRange)
-        {
-            printf("ERROR: String length must be no more than %d chars: ",maxRange);
-        }
-                
-    } while ( !(i>=minRange && i <=maxRange));
-    int j;
-    //inputString = &str;
-    if (i >= minRange && i <= maxRange) {
-            // Copy the valid input to the output string manually
-            for (j = 0; j < i; j++) {
-                inputString[j] = str[j];
-            }
-            inputString[i] = '\0'; // Ensure null-termination
-    }
-}
-
-void displayFormattedPhone(char* phoneNumber)
-{
-    int n = 0;
-    char numDigit[10] = {'0','1','2','3','4','5','6','7','8','9'};
-    int isValid = 1;
-    if(phoneNumber != NULL)
-    {
-        while(phoneNumber[n]!= '\0')
-        {
-            int isDigit = 0;
-            int j;
-            for(j=0;j<10;j++)
-            {
-                 if(phoneNumber[n] == numDigit[j])
-                {
-                    isDigit = 1;
-                    break;
-                }   
-            }
-            if(!isDigit)
-            {
-                isValid = 0;
-            }
-            n++;
-        }
-        if(n == 10 && isValid == 1)
-        {
-            printf("(%c%c%c)%c%c%c-%c%c%c%c",
-           phoneNumber[0], phoneNumber[1], phoneNumber[2],
-           phoneNumber[3], phoneNumber[4], phoneNumber[5],
-           phoneNumber[6], phoneNumber[7], phoneNumber[8], phoneNumber[9]);
-        }
-        else{
-            printf("(___)___-____");
-        }
-    }
-    else{
-        printf("(___)___-____");
-    }
-}
-#define BUFFER_SIZE 300
-
-int main() {
-    printf("*** Start of Tokenizing Words Demo ***\n"); 
-    char words[BUFFER_SIZE];    //create array with size 300 type char to store the words
-    char* nextWord = NULL;      //create pointer nextWord
-    int wordsCounter;           //create wordsCounter type int
-    do {
-        printf("Type a few words separated by space (q - to quit):\n");
-        fgets (words, BUFFER_SIZE, stdin);      //get the string store each character into words array
-        words [strlen(words) - 1] = '\0';       //set the end of the arrays is '\0' means ends of line
-        //check if the words != 'q', go to inside the if loop
-        if (strcmp(words, "q") != 0)            
-        { 
-            
-            nextWord = strtok(words, " ");
-            wordsCounter = 1;
-            while (nextWord) {
-                printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord); 
-                nextWord = strtok(NULL, " ");
-            }
-        }
-    } while (strcmp(words, "q") != 0);
-    printf("*** End of Tokenizing Words Demo ***\n\n");
+    // !!! DO NOT MODIFY THE INITIALIZATION OF THE "chorus" VARIABLE !!!
+    struct Song chorus = { "We Are The World", "Various Artists", {
+        {"We", 1}, {"are", 1}, {"the", 1}, {"world", 4}, {"We", 1}, {"are", 1}, {"the", 1}, {"children", 4},
+        {"We", 1}, {"are", 1}, {"the", 1}, {"ones", 1}, {"who", 1}, {"make", 1}, {"a", 1}, {"brighter", 2},
+        {"day", 2}, {"so", 1},{"lets", 1}, {"start", 1}, {"giving", 4} }, 21 };
+    int totalSeconds, minutes, seconds;
+    totalSeconds = karaoke(&chorus);
+    convertMinSec(totalSeconds, &minutes, &seconds);
+    printf("Duration: %02d:%02d\n\n", minutes, seconds);
     return 0;
 }
+
+    // ###### [EXPECTED OUTPUT] ######
+   // We Are The World (by:Various Artists)
+   // 
+   // We are the world ...
+   // We are the children ...
+   // We are the ones who make a brighter day so lets start giving ...
+   // Duration: 00:32
+
